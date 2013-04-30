53a5ce53f24daff486988ea0f0a72fd0194817d2
pratheep <pratheep.velicherla@gmail.com>
2013-04-30 20:43:03 +0530

    to_sym undefined array issue fixed and as of now supporting only one package for tag
    to_sym undefined array issue fixed and as of now supporting only one package for tag

--- lib/cloudfront_helper.rb
+++ lib/cloudfront_helper.rb
@@ -1,14 +1,14 @@
 module CloudfrontHelper
 	def include_cloudfront_css(*packages)
 		options = packages.extract_options!
-		css = include_stylesheets(packages, options)
+		css = include_stylesheets(packages[0], options)
 		css = css.gsub("/#{Jammit.package_path}/#{packages}.css","/#{packages}.css") if enabled?
 		css
 	end
 
 	def include_cloudfront_js(*packages)
 		options = packages.extract_options!
-		js = include_javascripts(packages, options)
+		js = include_javascripts(packages[0], options)
 		js = js.gsub("/#{Jammit.package_path}/#{packages}.js","/#{packages}.js") if enabled?
 		js
 	end
