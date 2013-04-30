79d4591f74c2c21447014f040986ee18b9bf89f9
pratheep <pratheep.velicherla@gmail.com>
2013-04-30 19:20:29 +0530

    other options supported like jammit
    other options supported like jammit

--- lib/cloudfront_helper.rb
+++ lib/cloudfront_helper.rb
@@ -1,12 +1,14 @@
 module CloudfrontHelper
 	def include_cloudfront_css(*packages)
-		css = include_stylesheets(*packages)
+		options = packages.extract_options!
+		css = include_stylesheets(packages, options)
 		css = css.gsub("/#{Jammit.package_path}/#{packages}.css","/#{packages}.css") if enabled?
 		css
 	end
 
 	def include_cloudfront_js(*packages)
-		js = include_javascripts(*packages)
+		options = packages.extract_options!
+		js = include_javascripts(packages, options)
 		js = js.gsub("/#{Jammit.package_path}/#{packages}.js","/#{packages}.js") if enabled?
 		js
 	end
